a31f3e58b70cc8010c6502b418935a3360f358b1
lhzsdnu <2383376138@qq.com>
2018-09-12 00:07:42 +0800

    增加新的示例

--- src/main/java/com/pinyougou/solr/controller/MusicRepository.java
+++ src/main/java/com/pinyougou/solr/controller/MusicRepository.java
@@ -1,10 +1,18 @@
 package com.pinyougou.solr.controller;
 
 import com.pinyougou.solr.pojo.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
 public interface MusicRepository extends SolrCrudRepository<Item, String> {
 
-
+    /**
+     * 分页模糊查询
+     * @param title
+     * @param pageable
+     * @return
+     */
+    Page<Item> findByTitleContaining(String title, Pageable pageable);
 
 }
--- src/main/java/com/pinyougou/solr/controller/TestSolrTemplateController.java
+++ src/main/java/com/pinyougou/solr/controller/TestSolrTemplateController.java
@@ -2,9 +2,14 @@ package com.pinyougou.solr.controller;
 
 import com.pinyougou.solr.pojo.Item;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -63,6 +68,67 @@ public class TestSolrTemplateController {
     public void testDelete(){
         musicRepository.deleteById("1");
     }
+
+    @RequestMapping("/addList")
+    public void testAddList(){
+        List<Item> list=new ArrayList();
+        for(int i=0;i<100;i++){
+            Item item=new Item();
+            item.setId(String.valueOf(i+1));
+            item.setBrand("华为");
+            item.setCategory("手机");
+            item.setGoodsId(1L);
+            item.setSeller("华为2号专卖店");
+            item.setTitle("华为Mate"+i);
+            item.setPrice(2000D);
+            list.add(item);
+        }
+        musicRepository.saveAll(list);
+    }
+
+    @RequestMapping("/pageQuery")
+    public void testPageQuery(){
+
+        //Query query=new SimpleQuery("*:*");
+        //query.setOffset(20L);//开始索引（默认0）
+        //query.setRows(20);//每页记录数(默认10)
+
+        Pageable pageable = PageRequest.of(1, 20);
+        Page<Item> page = musicRepository.findAll(pageable);
+        System.out.println("总记录数："+page.getTotalElements());
+        System.out.println("总分页数："+page.getTotalPages());
+
+        List<Item> list = page.getContent();
+        //显示记录数据
+        for(Item item:list){
+            System.out.println(item.getTitle() +"====="+item.getPrice());
+        }
+
+    }
+
+    @RequestMapping("/deleteAll")
+    public void testDeleteAll(){
+        musicRepository.deleteAll();
+    }
+
+
+    @RequestMapping("/pageQueryMutil")
+    public void testPageQueryMutil(){
+
+        Pageable pageable = PageRequest.of(0, 100);
+        Page<Item> page = musicRepository.findByTitleContaining("5",pageable);
+        System.out.println("总记录数："+page.getTotalElements());
+        System.out.println("总分页数："+page.getTotalPages());
+
+        List<Item> list = page.getContent();
+        //显示记录数据
+        for(Item item:list){
+            System.out.println(item.getTitle() +"====="+item.getPrice());
+        }
+    }
+
+
+
 
 
 
